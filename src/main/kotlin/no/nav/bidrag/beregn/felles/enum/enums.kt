ac3ecc22927727522e583fcd02eaee3cf8955e95
LarsOttoHaugen <lars.otto.haugen@nav.no>
2020-04-21 13:22:19 +0200

    Gjort om tester til Java, flyttet enums og fjernet overflødig kode

---
+++ src/main/kotlin/no/nav/bidrag/beregn/felles/enum/enums.kt
@@ -0,0 +1,20 @@
+package no.nav.bidrag.beregn.felles.enum
+
+enum class BostatusKode {
+  ALENE,
+  MED_FORELDRE,
+  MED_ANDRE_ENN_FORELDRE,
+  ENSLIG_ASYLANT
+}
+
+enum class SivilstandKode {
+  GIFT,
+  ENSLIG
+}
+
+enum class AvvikType {
+  PERIODER_OVERLAPPER,
+  PERIODER_HAR_OPPHOLD,
+  NULL_VERDI_I_DATO,
+  DATO_FRA_ETTER_DATO_TIL
+}
