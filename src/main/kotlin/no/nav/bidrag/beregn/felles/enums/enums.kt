609156709fe9a3c1c0b8d758ba7f5e49c2fccd13
rinnan17 <magnus.rinnan@nav.no>
2021-01-20 15:36:49 +0100

    Nye enums lagt til for resultatkode for forholdsmessig fordeling

--- src/main/kotlin/no/nav/bidrag/beregn/felles/enums/enums.kt
+++ src/main/kotlin/no/nav/bidrag/beregn/felles/enums/enums.kt
@@ -45,18 +45,20 @@ enum class Rolle {
 }
 
 enum class ResultatKode {
-  KOSTNADSBEREGNET_BIDRAG,                                   // Kostnadsberegnet bidrag
-  BARNET_ER_SELVFORSORGET,                                   // Barnet er selvforsørget
-  BIDRAG_REDUSERT_AV_EVNE,                                   // Bidrag redusert pga ikke full evne
-  INGEN_EVNE,                                                // BP har 0.- i bidragsevne, bidrag satt til 0.-
-  BIDRAG_REDUSERT_TIL_25_PROSENT_AV_INNTEKT,                 // Maks 25% av inntekt
-  BIDRAG_SATT_TIL_BARNETILLEGG_BP,                           // BarnetilleggBP er høyere enn beregnet bidrag
-  BIDRAG_SATT_TIL_UNDERHOLDSKOSTNAD_MINUS_BARNETILLEGG_BM,   // Beregnet bidrag er lavere enn underholdskostnad minus barnetilleggBM
-  BIDRAG_SATT_TIL_BARNETILLEGG_FORSVARET,                    // Barnebidrag settes likt barnetillegg fra forsvaret
-  DELT_BOSTED,                                               // Barnet bor like mye hos begge foreldre
-  BEGRENSET_REVURDERING,                                     // Beregnet bidrag er større enn forskuddsats, settes lik forskuddssats
-  BARNEBIDRAG_IKKE_BEREGNET_DELT_BOSTED,                     // Barnet har delt bosted og BPs andel av U er under 50%, bidrag skal ikke beregnes
-  SAERTILSKUDD_INNVILGET,                                    // Resultat av beregning av særtilskudd
-  SAERTILSKUDD_IKKE_FULL_BIDRAGSEVNE,                        // Resultat av beregning av særtilskudd
-  BEGRENSET_EVNE_FLERE_SAKER_UTFOER_FORHOLDSMESSIG_FORDELING // Resultat av beregning av barnebidrag, angir at det må gjøres en forholdsmessig fordeling
+  KOSTNADSBEREGNET_BIDRAG,                                    // Kostnadsberegnet bidrag
+  BARNET_ER_SELVFORSORGET,                                    // Barnet er selvforsørget
+  BIDRAG_REDUSERT_AV_EVNE,                                    // Bidrag redusert pga ikke full evne
+  INGEN_EVNE,                                                 // BP har 0.- i bidragsevne, bidrag satt til 0.-
+  BIDRAG_REDUSERT_TIL_25_PROSENT_AV_INNTEKT,                  // Maks 25% av inntekt
+  BIDRAG_SATT_TIL_BARNETILLEGG_BP,                            // BarnetilleggBP er høyere enn beregnet bidrag
+  BIDRAG_SATT_TIL_UNDERHOLDSKOSTNAD_MINUS_BARNETILLEGG_BM,    // Beregnet bidrag er lavere enn underholdskostnad minus barnetilleggBM
+  BIDRAG_SATT_TIL_BARNETILLEGG_FORSVARET,                     // Barnebidrag settes likt barnetillegg fra forsvaret
+  DELT_BOSTED,                                                // Barnet bor like mye hos begge foreldre
+  BEGRENSET_REVURDERING,                                      // Beregnet bidrag er større enn forskuddsats, settes lik forskuddssats
+  BARNEBIDRAG_IKKE_BEREGNET_DELT_BOSTED,                      // Barnet har delt bosted og BPs andel av U er under 50%, bidrag skal ikke beregnes
+  SAERTILSKUDD_INNVILGET,                                     // Resultat av beregning av særtilskudd
+  SAERTILSKUDD_IKKE_FULL_BIDRAGSEVNE,                         // Resultat av beregning av særtilskudd
+  BEGRENSET_EVNE_FLERE_SAKER_UTFOER_FORHOLDSMESSIG_FORDELING, // Resultat av beregning av barnebidrag, angir at det må gjøres en forholdsmessig fordeling
+  FORHOLDSMESSIG_FORDELING_INGEN_ENDRING,                     // Beregning av forholdsmessig fordeling er utført og det er ingen endringer på bidragsbeløp
+  FORHOLDSMESSIG_FORDELING_BIDRAGSBELOP_ENDRET                // Beregning av forholdsmessig fordeling er utført og det er beregnet nytt bidragsbeløp
 }
