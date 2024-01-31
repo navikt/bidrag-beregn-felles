945045efe0b0f9729522c49577c9ff35113dbf9d
Ugur Cenar <cenar93@gmail.com>
2024-01-31 07:57:45 +0100

    Opprett versjon fil

---
+++ bidrag-inntekt/src/main/kotlin/no/nav/bidrag/inntekt/util/VersionProvider.kt
@@ -0,0 +1,10 @@
+package no.nav.bidrag.inntekt.util
+
+class VersionProvider {
+    companion object {
+        val APP_VERSJON get() = hentFil("/versjon.txt").readText().trim()
+    }
+}
+private fun hentFil(filsti: String) = VersionProvider::class.java.getResource(
+    filsti,
+) ?: throw RuntimeException("Fant ingen fil på sti $filsti")
---
+++ bidrag-inntekt/src/test/kotlin/no/nav/bidrag/inntekt/util/VersionProviderTest.kt
@@ -0,0 +1,12 @@
+package no.nav.bidrag.inntekt.util
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class VersionProviderTest {
+
+    @Test
+    fun `Skal hente versjon fra fil`() {
+        VersionProvider.APP_VERSJON shouldBe "2024.01.23.3123213123 - ORIGINAL_COMMIT_HASH"
+    }
+}
