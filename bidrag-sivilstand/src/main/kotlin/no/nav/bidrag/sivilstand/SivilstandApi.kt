b20ea1720a10713db16dbb5d6ba9723bd5873969
Magnus Rinnan <magnus.rinnan@nav.no>
2024-02-01 13:50:11 +0100

    Tilpasniinger SivilstandApi

--- bidrag-sivilstand/src/main/kotlin/no/nav/bidrag/sivilstand/SivilstandApi.kt
+++ bidrag-sivilstand/src/main/kotlin/no/nav/bidrag/sivilstand/SivilstandApi.kt
@@ -3,18 +3,17 @@ package no.nav.bidrag.sivilstand
 import no.nav.bidrag.sivilstand.response.SivilstandBeregnet
 import no.nav.bidrag.sivilstand.service.SivilstandService
 import no.nav.bidrag.transport.behandling.grunnlag.response.SivilstandGrunnlagDto
-import org.springframework.stereotype.Service
 import java.time.LocalDate
 
 /**
  * SivilstandApi eksponerer api for å beregne tidlinje for sivilstand.
  *
  */
-@Service
 class SivilstandApi {
-    private val service = SivilstandService()
-
-    fun beregn(virkningstidspunkt: LocalDate, sivilstandGrunnlagDtoListe: List<SivilstandGrunnlagDto>): SivilstandBeregnet {
-        return service.beregn(virkningstidspunkt, sivilstandGrunnlagDtoListe)
+    companion object {
+        private val service = SivilstandService()
+        fun beregn(virkningstidspunkt: LocalDate, sivilstandGrunnlagDtoListe: List<SivilstandGrunnlagDto>): SivilstandBeregnet {
+            return service.beregn(virkningstidspunkt, sivilstandGrunnlagDtoListe)
+        }
     }
 }
