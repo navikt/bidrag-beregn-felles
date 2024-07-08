083f7bdacd8959f38d759c5e0010685fe71a61fd
Magnus Rinnan <magnus.rinnan@nav.no>
2024-07-08 13:08:13 +0200

    Justeringer pluss flere tester boforhold voksne

--- bidrag-boforhold/src/test/kotlin/no/nav/bidrag/boforhold/service/BoforholdAndreVoksneServiceV3Test.kt
+++ bidrag-boforhold/src/test/kotlin/no/nav/bidrag/boforhold/service/BoforholdAndreVoksneServiceV3Test.kt
@@ -39,4 +39,43 @@ internal class BoforholdAndreVoksneServiceV3Test {
             resultat[2].kilde shouldBe Kilde.OFFENTLIG
         }
     }
+
+    @Test
+    fun `Test voksne i husstanden offentlig og manuell periode`() {
+        boforholdAndreVoksneService = BoforholdAndreVoksneService()
+        val mottatteBoforhold = TestUtil.byggBorMedAndreVoksneOffentligeMauellePerioder1()
+        val virkningstidspunkt = LocalDate.of(2020, 9, 1)
+        val resultat = boforholdAndreVoksneService.beregnBoforholdAndreVoksne(virkningstidspunkt, mottatteBoforhold[0])
+        val resultat2 = boforholdAndreVoksneService.beregnBoforholdAndreVoksne(virkningstidspunkt, mottatteBoforhold[1])
+        val resultat3 = boforholdAndreVoksneService.beregnBoforholdAndreVoksne(virkningstidspunkt, mottatteBoforhold[2])
+
+        assertSoftly {
+            Assertions.assertNotNull(resultat)
+            // Beregning 1
+            resultat.size shouldBe 1
+            resultat[0].periodeFom shouldBe LocalDate.of(2020, 9, 1)
+            resultat[0].periodeTom shouldBe null
+            resultat[0].bostatus shouldBe Bostatuskode.BOR_MED_ANDRE_VOKSNE
+            resultat[0].kilde shouldBe Kilde.OFFENTLIG
+
+            // Beregning 2
+            resultat2.size shouldBe 2
+            resultat2[0].periodeFom shouldBe LocalDate.of(2020, 9, 1)
+            resultat2[0].periodeTom shouldBe LocalDate.of(2023, 8, 31)
+            resultat2[0].bostatus shouldBe Bostatuskode.BOR_MED_ANDRE_VOKSNE
+            resultat2[0].kilde shouldBe Kilde.OFFENTLIG
+
+            resultat2[1].periodeFom shouldBe LocalDate.of(2023, 9, 1)
+            resultat2[1].periodeTom shouldBe null
+            resultat2[1].bostatus shouldBe Bostatuskode.BOR_IKKE_MED_ANDRE_VOKSNE
+            resultat2[1].kilde shouldBe Kilde.MANUELL
+
+            // Beregning 3
+            resultat3.size shouldBe 1
+            resultat3[0].periodeFom shouldBe LocalDate.of(2020, 9, 1)
+            resultat3[0].periodeTom shouldBe null
+            resultat3[0].bostatus shouldBe Bostatuskode.BOR_MED_ANDRE_VOKSNE
+            resultat3[0].kilde shouldBe Kilde.OFFENTLIG
+        }
+    }
 }
