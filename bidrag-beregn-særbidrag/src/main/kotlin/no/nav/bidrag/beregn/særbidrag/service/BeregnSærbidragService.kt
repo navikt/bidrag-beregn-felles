79dbe4691a8734c8b873b9bf150d777905e92a8f
Ugur Alpay Cenar <cenar93@gmail.com>
2024-10-10 07:48:10 +0200

    Hent siste sjablonsats for FORSKUDDSSATS_BELØP

--- bidrag-beregn-særbidrag/src/main/kotlin/no/nav/bidrag/beregn/særbidrag/service/BeregnSærbidragService.kt
+++ bidrag-beregn-særbidrag/src/main/kotlin/no/nav/bidrag/beregn/særbidrag/service/BeregnSærbidragService.kt
@@ -97,7 +97,9 @@ internal class BeregnSærbidragService(
         delberegningUtgift: DelberegningUtgift,
         sjablonListe: SjablonListe = hentSjabloner(),
     ): Resultatkode? = if (vedtakstype == Vedtakstype.FASTSETTELSE) {
-        val forskuddssats = sjablonListe.sjablonSjablontallResponse.firstOrNull { it.typeSjablon == SjablonTallNavn.FORSKUDDSSATS_BELØP.id }
+        val forskuddssats = sjablonListe.sjablonSjablontallResponse
+            .filter { it.typeSjablon == SjablonTallNavn.FORSKUDDSSATS_BELØP.id }
+            .maxBy { it.datoTom ?: it.datoFom!! }
         if (delberegningUtgift.sumGodkjent < forskuddssats?.verdi) Resultatkode.GODKJENT_BELØP_ER_LAVERE_ENN_FORSKUDDSSATS else null
     } else {
         null
