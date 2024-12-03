0292554eb542af78d7c56d2f51763cbf23b5da58
Ugur Alpay Cenar <cenar93@gmail.com>
2024-12-03 11:59:34 +0100

    Feilfiks andre voksne i husstanden beregning

--- bidrag-boforhold/src/main/kotlin/no/nav/bidrag/boforhold/service/BoforholdAndreVoksneService.kt
+++ bidrag-boforhold/src/main/kotlin/no/nav/bidrag/boforhold/service/BoforholdAndreVoksneService.kt
@@ -752,7 +752,7 @@ internal class BoforholdAndreVoksneService {
         var periodeTom: LocalDate? = null
 
         sortertePerioder.forEachIndexed { indeks, bostatusperiode ->
-            if (indeks == 0 || bostatusperiode.periodeFom!!.isAfter(periodeTom?.plusDays(1))) {
+            if (indeks == 0 || periodeTom != null && bostatusperiode.periodeFom!!.isAfter(periodeTom?.plusDays(1))) {
                 if (indeks != 0) {
                     resultatliste.add(Bostatus(periodeFom, periodeTom, Bostatuskode.BOR_MED_ANDRE_VOKSNE, Kilde.OFFENTLIG))
                     periodeFom = null
