a967080420b8c38ec86ab67457421757fd194a17
Ugur Alpay Cenar <cenar93@gmail.com>
2025-03-17 13:50:11 +0100

    Filtrering av siste manuelle vedtak

---
+++ bidrag-vedtak/src/main/kotlin/no/nav/bidrag/beregn/vedtak/Felles.kt
@@ -0,0 +1,67 @@
+package no.nav.bidrag.beregn.vedtak
+
+import no.nav.bidrag.transport.behandling.vedtak.response.VedtakForStønad
+import no.nav.bidrag.transport.behandling.vedtak.response.VedtakPeriodeDto
+import no.nav.bidrag.transport.behandling.vedtak.response.søknadsid
+import java.math.BigDecimal
+
+data class Vedtaksdetaljer(var erOmgjort: Boolean = false, val vedtak: VedtakForStønad, val periode: VedtakPeriodeDto)
+
+class Vedtaksiterator(vedtakssamling: Collection<Vedtaksdetaljer>) : Iterator<Vedtaksdetaljer> {
+
+    private val iterator: Iterator<Vedtaksdetaljer> = vedtakssamling.asSequence().sortedByDescending { it.periode.delytelseId }.iterator()
+    private var nesteVedtak: Vedtaksdetaljer? = null
+    private var omgjorteVedtak = emptySet<Long>()
+
+    init {
+        forberedeNeste()
+    }
+
+    override fun hasNext(): Boolean = nesteVedtak != null
+
+    override fun next(): Vedtaksdetaljer {
+        if (!hasNext()) {
+            throw NoSuchElementException("Har ikke flere vedtak å iterere over.")
+        }
+        val neste = nesteVedtak
+        forberedeNeste()
+        return neste!!
+    }
+
+    private fun forberedeNeste() {
+        while (iterator.hasNext()) {
+            val vedtaksdetaljer = iterator.next()
+            if (vedtaksdetaljer.vedtak.stønadsendring.erEndring() && vedtaksdetaljer.vedtak.erKlage()) {
+                omgjorteVedtak.plus(vedtaksdetaljer.vedtak.idTilOmgjortVedtak())
+            }
+            vedtaksdetaljer.erOmgjort = omgjorteVedtak.contains(vedtaksdetaljer.vedtak.idTilOmgjortVedtak())
+            nesteVedtak = vedtaksdetaljer
+            return
+        }
+
+        nesteVedtak = null
+    }
+
+    fun hoppeTilPåklagetVedtak(referanseTilPåklagetVedtak: Long) {
+        while (nesteVedtak != null && nesteVedtak!!.vedtak.søknadsid != referanseTilPåklagetVedtak) {
+            forberedeNeste()
+        }
+    }
+
+    fun hoppeTilBeløp(beløp: BigDecimal?) {
+        while (nesteVedtak != null && !erSammeBeløp(nesteVedtak!!.periode.beløp, beløp)) {
+            forberedeNeste()
+        }
+    }
+
+    fun erSammeBeløp(beløpA: BigDecimal?, beløpB: BigDecimal?): Boolean {
+        if (beløpA == null) return beløpB == null
+        return beløpB != null && beløpA.compareTo(beløpB) == 0
+    }
+
+    fun hoppeTilOmgjortVedtak(idTilOmgjortVedtak: Long) {
+        while (nesteVedtak != null && nesteVedtak!!.vedtak.vedtaksid != idTilOmgjortVedtak) {
+            forberedeNeste()
+        }
+    }
+}
