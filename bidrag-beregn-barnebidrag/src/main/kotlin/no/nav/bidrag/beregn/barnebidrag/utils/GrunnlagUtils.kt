04ebb961ba9864c9d648ea8f7ebcf8b4a2c00cac
Ugur Alpay Cenar <cenar93@gmail.com>
2025-05-21 13:49:11 +0200

    Aldersjustering sjekk for nyeste ident for kravhaver (#592)
    * Aldersjustering sjekk for nyeste ident for kravhaver
    
    * Aldersjustering sjekk for nyeste ident for kravhaver

--- bidrag-beregn-barnebidrag/src/main/kotlin/no/nav/bidrag/beregn/barnebidrag/utils/GrunnlagUtils.kt
+++ bidrag-beregn-barnebidrag/src/main/kotlin/no/nav/bidrag/beregn/barnebidrag/utils/GrunnlagUtils.kt
@@ -1,5 +1,11 @@
 package no.nav.bidrag.beregn.barnebidrag.utils
 
+import no.nav.bidrag.commons.util.IdentUtils
+import no.nav.bidrag.domene.ident.Personident
+import no.nav.bidrag.transport.behandling.felles.grunnlag.BaseGrunnlag
+import no.nav.bidrag.transport.behandling.felles.grunnlag.GrunnlagDto
+import no.nav.bidrag.transport.behandling.felles.grunnlag.hentAllePersoner
+import no.nav.bidrag.transport.behandling.felles.grunnlag.personIdent
 import no.nav.bidrag.transport.behandling.stonad.response.StønadPeriodeDto
 import no.nav.bidrag.transport.behandling.vedtak.response.VedtakPeriodeDto
 import java.time.YearMonth
@@ -9,3 +15,11 @@ fun List<VedtakPeriodeDto>.hentSisteLøpendePeriode() = maxByOrNull { it.periode
 
 fun List<StønadPeriodeDto>.hentSisteLøpendePeriode() = maxByOrNull { it.periode.fom }
     ?.takeIf { it.periode.til == null || it.periode.til!!.isAfter(YearMonth.now()) }
+
+fun List<GrunnlagDto>.hentPersonForNyesteIdent(identUtils: IdentUtils, identFraVedtak: Personident): BaseGrunnlag? {
+    val kravhaverNyesteIdent = identUtils.hentNyesteIdent(identFraVedtak)
+    return hentAllePersoner().find {
+        val personNyesteIdent = identUtils.hentNyesteIdent(Personident(it.personIdent!!))
+        personNyesteIdent.verdi == kravhaverNyesteIdent.verdi
+    }
+}
