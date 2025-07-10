25ea31fdb48c22369450ab0e9beaf3dadf93f918
Ugur Alpay Cenar <cenar93@gmail.com>
2025-07-10 10:28:38 +0200

    Manuell aldersjustering ignorer begrunnelse VEDTAK_GRUNNLAG_HENTES_FRA_HAR_PERIODE_MED_FOM_DATO_ETTER_ALDERSJUSTERINGEN (#641)
    * Manuell aldersjustering ignorer begrunnelse VEDTAK_GRUNNLAG_HENTES_FRA_HAR_PERIODE_MED_FOM_DATO_ETTER_ALDERSJUSTERINGEN
    
    * Manuell aldersjustering ignorer begrunnelse LØPENDE_PERIODE_FRA_OG_MED_DATO_ER_LIK_ELLER_ETTER_ALDERSJUSTERING

--- bidrag-beregn-barnebidrag/src/main/kotlin/no/nav/bidrag/beregn/barnebidrag/service/external/BeregningBeløpshistorikkConsumer.kt
+++ bidrag-beregn-barnebidrag/src/main/kotlin/no/nav/bidrag/beregn/barnebidrag/service/external/BeregningBeløpshistorikkConsumer.kt
@@ -3,6 +3,6 @@ package no.nav.bidrag.beregn.barnebidrag.service.external
 import no.nav.bidrag.transport.behandling.belopshistorikk.request.HentStønadHistoriskRequest
 import no.nav.bidrag.transport.behandling.belopshistorikk.response.StønadDto
 
-interface BeregningStønadConsumer {
+interface BeregningBeløpshistorikkConsumer {
     fun hentHistoriskeStønader(hentStønadHistoriskRequest: HentStønadHistoriskRequest): StønadDto?
 }
