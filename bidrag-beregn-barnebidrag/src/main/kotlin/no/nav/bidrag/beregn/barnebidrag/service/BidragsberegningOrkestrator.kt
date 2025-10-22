52011feab9fd3976038692863178b2a3ba42e404
Ugur Alpay Cenar <cenar93@gmail.com>
2025-10-22 09:48:46 +0200

    Lagt til utførBidragsberegningV3 i BidragsberegningOrkestrator

--- bidrag-beregn-barnebidrag/src/main/kotlin/no/nav/bidrag/beregn/barnebidrag/service/BidragsberegningOrkestrator.kt
+++ bidrag-beregn-barnebidrag/src/main/kotlin/no/nav/bidrag/beregn/barnebidrag/service/BidragsberegningOrkestrator.kt
@@ -26,6 +26,142 @@ import org.springframework.stereotype.Service
 @Import(BeregnBarnebidragApi::class, OmgjøringOrkestrator::class)
 class BidragsberegningOrkestrator(private val barnebidragApi: BeregnBarnebidragApi, private val omgjøringOrkestrator: OmgjøringOrkestrator) {
 
+    fun utførBidragsberegningV3(request: BidragsberegningOrkestratorRequestV2): BidragsberegningOrkestratorResponseV2 {
+        when (request.beregningstype) {
+            Beregningstype.BIDRAG -> {
+                secureLogger.debug { "Utfører bidragsberegning for request: $request" }
+                val respons = request.beregningBarn.map {
+                    try {
+                        val beregningResultat = if (request.erDirekteAvslag) {
+                            barnebidragApi.opprettAvslag(
+                                it.tilBeregnGrunnlagV1(request.grunnlagsliste).leggTilÅpenSluttperiodeHvisDirekteAvslagBeregning(),
+                            )
+                        } else {
+                            barnebidragApi.beregn(
+                                beregnGrunnlag = it.tilBeregnGrunnlagV1(request.grunnlagsliste),
+                            )
+                        }
+                        BidragsberegningResultatBarnV2(
+                            it.søknadsbarnreferanse,
+                            listOf(
+                                ResultatVedtakV2(
+                                    periodeListe = beregningResultat.beregnetBarnebidragPeriodeListe,
+                                    delvedtak = false,
+                                    omgjøringsvedtak = false,
+                                    vedtakstype = Vedtakstype.ENDRING,
+                                ),
+                            ),
+                        ) to beregningResultat.grunnlagListe
+                    } catch (e: Exception) {
+                        BidragsberegningResultatBarnV2(
+                            it.søknadsbarnreferanse,
+                            emptyList(),
+                            beregningsfeil = e,
+                        ) to request.grunnlagsliste
+                    }
+                }
+
+                secureLogger.debug { "Resultat av bidragsberegning: $respons" }
+                return BidragsberegningOrkestratorResponseV2(
+                    respons.flatMap { it.second },
+                    respons.map { it.first },
+                )
+            }
+            Beregningstype.OMGJØRING -> {
+                secureLogger.debug { "Utfører omgjøringsberegning for request: $request" }
+                val respons = request.beregningBarn.map {
+                    try {
+                        val klageberegningResultat = if (request.erDirekteAvslag) {
+                            // Avslagsperiode skal alltid være løpende hvis det ikke kommer noe periode etter opphøret (feks ved etterfølgende vedtak i orkestrering)
+                            barnebidragApi.opprettAvslag(
+                                it.tilBeregnGrunnlagV1(request.grunnlagsliste).leggTilÅpenSluttperiodeHvisDirekteAvslagBeregning(),
+                            )
+                        } else {
+                            barnebidragApi.beregn(
+                                beregnGrunnlag = it.tilBeregnGrunnlagV1(request.grunnlagsliste),
+                            )
+                        }
+                        BidragsberegningResultatBarnV2(
+                            it.søknadsbarnreferanse,
+                            listOf(
+                                ResultatVedtakV2(
+                                    periodeListe = klageberegningResultat.beregnetBarnebidragPeriodeListe,
+                                    delvedtak = false,
+                                    omgjøringsvedtak = true,
+                                    vedtakstype = Vedtakstype.KLAGE,
+                                ),
+                            ),
+                        ) to klageberegningResultat.grunnlagListe
+                    } catch (e: Exception) {
+                        BidragsberegningResultatBarnV2(
+                            it.søknadsbarnreferanse,
+                            emptyList(),
+                            beregningsfeil = e,
+                        ) to request.grunnlagsliste
+                    }
+                }
+                secureLogger.debug { "Resultat av omgjøringsberegning: $respons" }
+                return BidragsberegningOrkestratorResponseV2(
+                    respons.flatMap { it.second },
+                    respons.map { it.first },
+                )
+            }
+            Beregningstype.OMGJØRING_ENDELIG -> {
+                secureLogger.debug { "Utfører endelig omgjøringsberegning for request: $request" }
+                val respons = request.beregningBarn.map {
+                    try {
+                        val klageberegningResultat = if (request.erDirekteAvslag) {
+                            barnebidragApi.opprettAvslag(
+                                it.tilBeregnGrunnlagV1(request.grunnlagsliste).leggTilÅpenSluttperiodeHvisDirekteAvslagBeregning(),
+                            )
+                        } else {
+                            barnebidragApi.beregn(
+                                beregnGrunnlag = it.tilBeregnGrunnlagV1(request.grunnlagsliste),
+                            )
+                        }
+                        val endeligKlageberegningResultat = omgjøringOrkestrator.utførOmgjøringEndelig(
+                            omgjøringResultat = klageberegningResultat,
+                            omgjøringGrunnlag = it.tilBeregnGrunnlagV1(request.grunnlagsliste),
+                            omgjøringOrkestratorGrunnlag =
+                            it.omgjøringOrkestratorGrunnlag ?: throw IllegalArgumentException("klageOrkestratorGrunnlag må være angitt"),
+                        )
+                        BidragsberegningResultatBarnV2(
+                            it.søknadsbarnreferanse,
+                            endeligKlageberegningResultat.map {
+                                ResultatVedtakV2(
+                                    periodeListe = it.resultat.beregnetBarnebidragPeriodeListe,
+                                    delvedtak = it.delvedtak,
+                                    grunnlagslisteDelvedtak = if (it.delvedtak) it.resultat.grunnlagListe else emptyList(),
+                                    omgjøringsvedtak = it.omgjøringsvedtak,
+                                    beregnet = it.beregnet,
+                                    vedtakstype = it.vedtakstype,
+                                )
+                            },
+                        ) to
+                            endeligKlageberegningResultat.flatMap {
+                                if (!it.delvedtak && !it.omgjøringsvedtak) {
+                                    it.resultat.grunnlagListe
+                                } else {
+                                    emptyList()
+                                }
+                            }
+                    } catch (ex: Exception) {
+                        BidragsberegningResultatBarnV2(
+                            it.søknadsbarnreferanse,
+                            emptyList(),
+                            beregningsfeil = ex,
+                        ) to request.grunnlagsliste
+                    }
+                }
+                secureLogger.debug { "Resultat av endelig klageberegning: $respons" }
+                return BidragsberegningOrkestratorResponseV2(
+                    respons.flatMap { it.second },
+                    respons.map { it.first },
+                )
+            }
+        }
+    }
+
     // TODO: Ikke gjør endringer her. Lag en utførBidragsberegningV3 som tar hensyn til FF. utførBidragsberegningV3 skal ha samme api (input og output) som utførBidragsberegningV2
     fun utførBidragsberegningV2(request: BidragsberegningOrkestratorRequestV2): BidragsberegningOrkestratorResponseV2 {
         when (request.beregningstype) {
